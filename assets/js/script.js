9d5694ef21f0f874cf741b20f1869e367dccf288
Dev4Crypto <92862036+Dev4Crypto@users.noreply.github.com>
2022-07-25 17:40:50 +0530

    updated kyc warning text

--- assets/js/script.js
+++ assets/js/script.js
@@ -614,12 +614,12 @@ const fetchResponse =  await fetch(customURL,requestOptions);
     
 const edata = await fetchResponse.json(); 
 if(edata.success==false){
-    alertify.alert("Warning","Your wallet is not KYC verified.");
+    alertify.alert("Warning","Your wallet is not KYC verified.Please get KYC verification at kyc.sardisnetwork.com");
     return false;
 }
  if(edata.success==true){
      if(edata.kyc_status == false){
-        alertify.alert("Warning","Your wallet is not KYC verified.");
+        alertify.alert("Warning","Your wallet is not KYC verified.Please get KYC verification at kyc.sardisnetwork.com");
         return false;
      }
  }
